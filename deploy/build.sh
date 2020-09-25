9ce7d0798e838710ea235d8746ff8120df4aba83
qwertys318 <343604@gmail.com>
2020-09-25 18:14:46 +0700

    fix package name

--- deploy/build.sh
+++ deploy/build.sh
@@ -8,6 +8,6 @@ composer install --no-dev --no-interaction --no-cache
 composer dump-autoload -o
 composer dump-env prod
 rm -rf ./.git ./.env ./front
-docker build -f ../deploy/Dockerfile -t docker.pkg.github.com/qwertys318/keep-the-only-chance/fpm:${1} -t docker.pkg.github.com/qwertys318/keep-the-only-chance/fpm:latest  .
-docker push docker.pkg.github.com/qwertys318/keep-the-only-chance/fpm:${1}
-docker push docker.pkg.github.com/qwertys318/keep-the-only-chance/fpm:latest
+docker build -f ../deploy/Dockerfile -t docker.pkg.github.com/qwertys318/keep-the-only-chance/ktoc-fpm:${1} -t docker.pkg.github.com/qwertys318/keep-the-only-chance/ktoc-fpm:latest  .
+docker push docker.pkg.github.com/qwertys318/keep-the-only-chance/ktoc-fpm:${1}
+docker push docker.pkg.github.com/qwertys318/keep-the-only-chance/ktoc-fpm:latest
